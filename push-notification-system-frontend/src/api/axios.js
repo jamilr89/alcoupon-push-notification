a3603362f457b17b215f040f60f381678edd7fa1
jamilr89 <jamel89@gmail.com>
2025-11-28 01:03:35 +0300

    fix env value

--- push-notification-system-frontend/src/api/axios.js
+++ push-notification-system-frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const BASE_URL=process.env.REACT_APP_API_URL??'http://api'
-console.log("BASE_URL:",BASE_URL)
+// prefer REACT_APP_API_URL if provided, otherwise use same-origin
+const BASE_URL = process.env.REACT_APP_API_URL ?? '/';
+console.log("BASE_URL:", BASE_URL);
 
 export default axios.create({
     baseURL: BASE_URL
